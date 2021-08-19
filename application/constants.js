3e9b4fa25406fc8aa7bb1bdf6033935f86daf25e
nastish.n <nastish.novikova@yandex.ru>
2021-08-19 18:08:04 +0300

    added regex constants for checking url and email

--- application/constants.js
+++ application/constants.js
@@ -7,8 +7,20 @@ const mongoOptions = {
 };
 const mongoUrl = 'mongodb://localhost:27017/bitfilmsdb';
 
+// url and email regex
+const urlRegex = '^(?!mailto:)(?:(?:http|https|ftp)://)(?:\\S+(?::\\S*)?@)?(?:(?:(?:[1-9]\\d?|1\\d\\d|2[01]\\d|22[0-3])(?:\\.(?:1?\\d{1,2}|2[0-4]\\d|25[0-5])){2}(?:\\.(?:[0-9]\\d?|1\\d\\d|2[0-4]\\d|25[0-4]))|(?:(?:[a-z\\u00a1-\\uffff0-9]+-?)*[a-z\\u00a1-\\uffff0-9]+)(?:\\.(?:[a-z\\u00a1-\\uffff0-9]+-?)*[a-z\\u00a1-\\uffff0-9]+)*(?:\\.(?:[a-z\\u00a1-\\uffff]{2,})))|localhost)(?::\\d{2,5})?(?:(/|\\?|#)[^\\s]*)?$';
+const urlCheck = new RegExp(urlRegex, 'i');
+const emailRegex = '^[a-zA-Z0-9.!#$%&\'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:.[a-zA-Z0-9-]+)*$';
+const emailCheck = new RegExp(emailRegex);
+
+// error messages
+const loginErrorMessage = 'Неправильная почта или пароль';
+
 
 module.exports = {
   mongoOptions,
   mongoUrl,
+  urlCheck,
+  emailCheck,
+  loginErrorMessage,
 };
