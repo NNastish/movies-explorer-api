20b126152ec18114af468e7919ac7327a5dce663
nastish.n <nastish.novikova@yandex.ru>
2021-08-19 18:07:23 +0300

    defined authentication part

--- application/app.js
+++ application/app.js
@@ -7,6 +7,7 @@ const { requestLogger, errorLogger } = require('./middlewares/logger');
 const { checkCors } = require('./middlewares/cors');
 const { celebrate, Joi, errors } = require('celebrate');
 // TODO: add import of login, createUser functions
+const { login, createUser } = require('./controllers/usersController');
 const { auth } = require('./middlewares/auth');
 const { errorHandler } = require('./middlewares/errorHandler');
 
@@ -28,9 +29,20 @@ app.use(requestLogger);
 app.use(checkCors);
 
 // unprotected routes (signup && signin)
-app.post('/signup');
+app.post('/signup', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(8),
+    name: Joi.string().min(2).max(30).required(),
+  })
+}), createUser);
 
-app.post('/signin');
+app.post('/signin', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(8),
+  })
+}), login);
 
 // protection of other routes
 app.use(auth);
